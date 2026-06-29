c8c7a1b07f89da1c8c2030ef53761146f4973b3d
vedantpokale1-sys <vedantpokale1@gmail.com>
2026-06-29 19:49:58 +0530

    Add simple calculator to ved.py

--- ved.py
+++ ved.py
@@ -1,2 +1,18 @@
 print("im the boss")
 name = int(input("my name is the vedant"))
+a = int(input("enter the first number: "))
+b = int(input("enter the second number: "))
+operator = input("choose the following operator (+, -, *, /): ")
+if operator == "+":
+    print(a + b)
+elif operator == "-":
+    print(a - b)
+elif operator == "*":
+    print(a * b)
+elif operator == "/":
+    print(a / b)
+else:
+    print("invalid operator")
+
+
+
